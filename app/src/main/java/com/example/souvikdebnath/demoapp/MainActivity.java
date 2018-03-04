5dd4eeab35bc3cf9077e09bada4ad9892bb3d143
souvikdebnath <souvikdebnath59@gmail.com>
2018-03-04 10:50:25 -0500

    Build it

--- app/src/main/java/com/example/souvikdebnath/demoapp/MainActivity.java
+++ app/src/main/java/com/example/souvikdebnath/demoapp/MainActivity.java
@@ -1,4 +1,10 @@
 package com.example.souvikdebnath.demoapp;
+import android.content.Intent;
+//import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public static Button Register_Here;
+
+
+
+    public void OnclickButtonListener(){
+        Register_Here =(Button)findViewById(R.id.button);
+        Register_Here.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent("com.example.mohammadzakriya.tech_bgkapp.Secondactivity");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
+
